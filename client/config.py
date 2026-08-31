1ad13ed4ea01a823ac71c2d42cd85ea72eebfcd4
Arvind7248 <ak2171257@gmail.com>
2026-08-31 20:13:07 +0530

    New changes

--- client/config.py
+++ client/config.py
@@ -1,1 +1,7 @@
-API_URL="http://127.0.0.1:8000"
+<<<<<<< HEAD
+#API_URL="http://127.0.0.1:8000"
+API_URL="https://medical-assistance-rag-chatbot.onrender.com"
+=======
+#API_URL="http://127.0.0.1:8000
+API_URL="https://medical-assistance-rag-chatbot.onrender.com"
+>>>>>>> 9a016fe28b4d27899c91f92b346e056d9100c6c1
