9df27acc1e88b8a191e7899abb8dd3c41f3d5706
Arvind Kumar <24cd3008@rgipt.ac.in>
2026-08-31 20:15:47 +0530

    Remove merge conflict markers from config.py

--- client/config.py
+++ client/config.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-#API_URL="http://127.0.0.1:8000"
-API_URL="https://medical-assistance-rag-chatbot.onrender.com"
-=======
+
 #API_URL="http://127.0.0.1:8000
 API_URL="https://medical-assistance-rag-chatbot.onrender.com"
->>>>>>> 9a016fe28b4d27899c91f92b346e056d9100c6c1
+
